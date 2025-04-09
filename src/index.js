de46573e4bcb0929768f94fe6ee5af631a303d1f
Dri02 <daliarisla2002@gmail.com>
2025-04-09 09:46:00 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./routes.js");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
